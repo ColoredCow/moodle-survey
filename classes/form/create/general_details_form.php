78329ff62a38fd62ae07ec0ad2998626e4474400
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-07-23 19:00:16 +0530

    Added survey name input in the generic details form

--- classes/form/create/general_details_form.php
+++ classes/form/create/general_details_form.php
@@ -11,8 +11,11 @@ class general_details_form extends \customformlib {
 
         // Add custom HTML for form heading
         $mform->addElement('html', '<div class="form-section">');
+        $this->add_survey_name_field($mform);
         $this->add_survey_category_field($mform);
         $this->add_survey_description_field($mform);
+        $mform->addElement('html', '</div>');
+
 
         // Add action buttons
         $this->add_custom_action_buttons_helper(true, get_string('submit', 'local_moodle_survey'));
@@ -23,8 +26,8 @@ class general_details_form extends \customformlib {
         $iconurl = new \moodle_url('/local/moodle_survey/pix/plus-icon.svg');
 
         $mform->addElement('html', '<div class="form-group">');
-        $mform->addElement('html', '<label for="id_name">' . get_string('surveycategory', 'local_moodle_survey') . '</label>');
-        $mform->addElement('html', '<select id="id_name" name="status" class="form-control" required>');
+        $mform->addElement('html', '<label for="survey_category">' . get_string('surveycategory', 'local_moodle_survey') . '</label>');
+        $mform->addElement('html', '<select id="survey_category" name="status" class="form-control" required>');
         $mform->addElement('html', '<option value="0">' . get_string('inactive', 'local_moodle_survey') . '</option>');
         $mform->addElement('html', '<option value="1">' . get_string('active', 'local_moodle_survey') . '</option>');
         $mform->addElement('html', '</select>');
@@ -39,6 +42,12 @@ class general_details_form extends \customformlib {
         $mform->addElement('html', '<label for="id_description">' . get_string('surveydescription', 'local_moodle_survey') . '</label>');
         $mform->addElement('html', '<textarea id="id_description" placeholder="' . get_string('surveydescriptionplaceholder', 'local_moodle_survey') . '" name="description" wrap="virtual" rows="5" cols="100" class="form-control"></textarea>');
         $mform->addElement('html', '</div>');
+    }
+    
+    private function add_survey_name_field($mform) {
+        $mform->addElement('html', '<div class="form-group">');
+        $mform->addElement('html', '<label for="survey_name">' . get_string('surveyname', 'local_moodle_survey') . '</label>');
+        $mform->addElement('html', '<input id="survey_name" placeholder="' . get_string('surveynameplaceholder', 'local_moodle_survey') . '" name="name" class="form-control" />');
         $mform->addElement('html', '</div>');
     }
 }
