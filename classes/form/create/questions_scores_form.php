7558f2186322a214e82a8534d959d3fa0f6d77db
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-07-25 14:28:39 +0530

    fix question form

--- classes/form/create/questions_scores_form.php
+++ classes/form/create/questions_scores_form.php
@@ -28,20 +28,21 @@ class questions_scores_form extends \moodleform {
         $mform->addElement('html', '<div class="accordion-header general-details-section">');
         $mform->addElement('html', '<img src="' . $iconurl . '" alt="Icon" class="accordion-icon">');
         $mform->addElement('html', '<h5>Question <span id="question-number">' . $questionposition . '</span></h5>');
-        $mform->addElement('html', '</div><div class="accordion-body question-score-form">');
+        $mform->addElement('html', '</div>');
+        $mform->addElement('html', '<div class="accordion-body question-score-form">');
         $this->get_survey_question_field($mform, $index);
         $this->get_question_category_section($mform, $index);
-        // $mform->addElement('html', '<div class="new-option-section question-score-option-section" id="question-score-option-section">');
         $this->get_question_score_section($mform, $index);
-        // $mform->addElement('html', '</div>');
         $mform->addElement('html', '</div>');
         $mform->addElement('html', '</div>');
         $mform->addElement('html', '<div class="new-sections-container"></div>');
     }
     
     protected function get_question_score_section($mform, $index) {
-        $mform->addElement('text', 'question[' . $index . '][score][0]', get_string('score', 'local_moodle_survey'), 'min="1" max="10" class=""');
         $mform->addElement('html', '<div class="associated-option-section">');
+        
+        $mform->addElement('text', 'question[' . $index . '][score][0]', get_string('score', 'local_moodle_survey'), 'size="15" min="1" max="10" class=""');
+        $mform->setType('question[' . $index . '][option][0]', PARAM_INT);
         $mform->addRule('question[' . $index . '][score][0]', null, 'required', null, 'client');
         
         $mform->addElement('text', 'question[' . $index . '][option][0]', get_string('associatedoption', 'local_moodle_survey'), 'size="50" placeholder="' . get_string('interpretedasplaceholder', 'local_moodle_survey') . '"');
