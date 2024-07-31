7174275cbde3cbf26d63c006ff39dbbd74568a98
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-07-31 11:44:27 +0530

    fix(*): seprate out the taking survey option

--- templates/manage_survey_table.php
+++ templates/manage_survey_table.php
@@ -9,13 +9,15 @@ $table->head = [
     get_string('schools', 'local_moodle_survey'),
     get_string('responses', 'local_moodle_survey'),
     get_string('surveystatus', 'local_moodle_survey'),
+    'Taking survey',
 ];
 
 foreach ($surveys as $survey) {
-    // $editurl = new moodle_url('/local/moodle_survey/edit_survey.php', ['id' => $survey->id]);
-    $editurl = new moodle_url('/local/moodle_survey/modes_of_learning_survey/index.php');
+    $editurl = new moodle_url('/local/moodle_survey/edit_survey.php', ['id' => $survey->id]);
+    $takingsurvey = new moodle_url('/local/moodle_survey/modes_of_learning_survey/index.php');
     $deleteurl = new moodle_url('/local/moodle_survey/delete_survey.php', ['id' => $survey->id]);
     $surveyname = html_writer::link($editurl, $survey->name);
+    $takingsurvey = html_writer::link($takingsurvey, 'View');
     $surveycategory = $dbhelper->get_surver_category_by_id($survey->category_id);
     $surveycreatedon = new DateTime($survey->created_at);
     $surveycreatedondate = $surveycreatedon->format('Y-m-d');
@@ -28,6 +30,7 @@ foreach ($surveys as $survey) {
         format_string('0'),
         format_string('0'),
         format_string($survey->status),
+        $takingsurvey
     ];
 }
 
