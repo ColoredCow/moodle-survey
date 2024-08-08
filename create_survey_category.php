2d4d20639a0eaa41bffcb86f5c009978bc9093ae
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-08-08 17:39:30 +0530

    fix(*): fix the UI and code refactoring

--- create_survey_category.php
+++ create_survey_category.php
@@ -9,39 +9,81 @@ $pagetype = "create_survey_category";
 $PAGE->set_context($context);
 $PAGE->set_url(new moodle_url('/local/moodle_survey/create_survey_category.php'));
 $PAGE->set_title(get_string('createsurveycategory', 'local_moodle_survey'));
-$PAGE->set_heading(get_string('surveycategorypagetitle', 'local_moodle_survey'));
+
 echo $OUTPUT->header();
-$deleteurl = new \moodle_url('/local/moodle_survey/pix/delete-icon.svg');
-
-$categoryoptions['all'] = 'Select Category';
-
-echo html_writer::start_tag('form', ['method' => 'get', 'action' => $PAGE->url, 'id' => 'filter-form']);
-    echo html_writer::start_div('filter-form d-flex justify-content-around');
-        echo html_writer::empty_tag('input', ['type' => 'date', 'name' => 'createdon', 'placeholder' => get_string('createdat', 'local_moodle_survey'), 'class' => 'date-input']);
-        echo html_writer::empty_tag('input', ['type' => 'text', 'name' => 'search', 'value' => $search, 'placeholder' => get_string('search', 'local_moodle_survey'), 'class' => 'search-input']);
-    echo html_writer::end_div();
-echo html_writer::end_tag('form');
-
-$table = new html_table();
-$dbhelper = new \local_moodle_survey\model\survey();
-$surveycategories = $dbhelper->get_all_survey_categories();
-$table->head = [
-    get_string('category', 'local_moodle_survey'),
-    get_string('createdon', 'local_moodle_survey'),
-    get_string('action', 'local_moodle_survey'),
-];
-
-foreach ($surveycategories as $category) {
-    $table->data[] = [
-        html_writer::link(new moodle_url('/local/moodle_survey/create_survey.php', ['category' => $category->id]),  $category->label),
-        date('Y-m-d', strtotime($category->created_at)),
-        html_writer::link(new moodle_url($deleteurl), 
-            html_writer::tag('img', '', array('src' => $deleteurl, 'alt' => 'Icon', 'class' => 'plus-icon'))
-        ),
-    ];
-}
-
-echo html_writer::table($table);
-
+echo generate_page_header();
+echo generate_filter_form();
+echo generate_survey_table();
 
 require_once('includes/footer.php');
+
+/**
+ * Generates the page header including the create survey button and heading.
+ *
+ * @return string HTML content for the page header.
+ */
+function generate_page_header() {
+    $plusicon = new moodle_url('/local/moodle_survey/pix/plus-icon.svg');
+    $createurl = new moodle_url('/local/moodle_survey/create_survey.php');
+    $createbutton = html_writer::div(
+        html_writer::link(
+            $createurl,
+            html_writer::tag('img', '', ['src' => $plusicon, 'alt' => 'Icon', 'class' => 'plus-icon']) . ' ' . get_string('createsurvey', 'local_moodle_survey'),
+            ['class' => 'create-survey-button']
+        ),
+        'create-survey-button-container'
+    );
+    $heading = html_writer::tag('span', get_string('surveycategorypagetitle', 'local_moodle_survey'), ['class' => 'survey-name']);
+    $content = $heading . ' ' . $createbutton;
+    return html_writer::tag('div', $content, ['class' => 'survey-header']);
+}
+
+/**
+ * Generates the filter form for searching and filtering survey categories.
+ *
+ * @return string HTML content for the filter form.
+ */
+function generate_filter_form() {
+    global $PAGE, $search;
+
+    return html_writer::start_tag('form', ['method' => 'get', 'action' => $PAGE->url, 'id' => 'filter-form']) .
+            html_writer::start_div('filter-form d-flex justify-content-around') .
+            html_writer::empty_tag('input', ['type' => 'date', 'name' => 'createdon', 'placeholder' => get_string('createdat', 'local_moodle_survey'), 'class' => 'date-input']) .
+            html_writer::empty_tag('input', ['type' => 'text', 'name' => 'search', 'value' => $search, 'placeholder' => get_string('search', 'local_moodle_survey'), 'class' => 'search-input']) .
+            html_writer::end_div() .
+            html_writer::end_tag('form');
+}
+
+/**
+ * Generates the survey table with categories and actions.
+ *
+ * @return string HTML content for the survey table.
+ */
+function generate_survey_table() {
+    $table = new html_table();
+    $dbhelper = new \local_moodle_survey\model\survey();
+    $surveycategories = $dbhelper->get_all_survey_categories();
+    $deleteurl = new moodle_url('/local/moodle_survey/pix/delete-icon.svg');
+
+    $table->head = [
+        get_string('category', 'local_moodle_survey'),
+        get_string('createdon', 'local_moodle_survey'),
+        get_string('action', 'local_moodle_survey'),
+    ];
+
+    if(sizeof($surveycategories) > 0) {
+        foreach ($surveycategories as $category) {
+            $table->data[] = [
+                html_writer::link(new moodle_url('/local/moodle_survey/create_survey.php', ['category' => $category->id]),  $category->label),
+                date('Y-m-d', strtotime($category->created_at)),
+                html_writer::link(new moodle_url($deleteurl), 
+                    html_writer::tag('img', '', ['src' => $deleteurl, 'alt' => 'Icon', 'class' => 'plus-icon'])
+                ),
+            ];
+        }
+    } else {
+        echo html_writer::tag('div', get_string('nosurveysfound', 'local_moodle_survey'), ['class' => 'alert alert-info']);
+    }
+
+    return html_writer::table($table);
+}
--- manage_survey.php
+++ manage_survey.php
@@ -3,39 +3,78 @@
 require_once('../../config.php');
 require_login();
 
-$dbhelper = new \local_moodle_survey\model\survey();
-$context = context_system::instance();
-$PAGE->set_context($context);
-$PAGE->set_url(new moodle_url('/local/moodle_survey/manage_survey.php'));
-$PAGE->set_title(get_string('managesurvey', 'local_moodle_survey'));
-
-$search = optional_param('search', '', PARAM_RAW_TRIMMED);
-$status = optional_param('status', '', PARAM_ALPHA);
-$surveycategory = optional_param('category', '', PARAM_ALPHANUMEXT);
-$createdon = optional_param('createdon', '', PARAM_RAW_TRIMMED);
-$filters = [
-    'search' => $search,
-    'status' => $status,
-    'surveycategory' => $surveycategory,
-    'createdon' => $createdon,
-];
-
-$PAGE->requires->css(new moodle_url('/local/moodle_survey/css/styles.css'));
-$PAGE->requires->js(new moodle_url('/local/moodle_survey/js/tabs.js'));
-$PAGE->requires->js(new moodle_url('/local/moodle_survey/js/forms.js'));
-
+initialize_page();
 echo $OUTPUT->header();
 
-// Include the HTML for the survey management interface
-include(__DIR__ . '/templates/manage_survey_header.php');
+$filters = get_filters();
 
-// Fetch and display surveys based on filters
-$surveys = $dbhelper->get_surveys($filters);
+// Fetch surveys
+$surveys = fetch_surveys($filters);
 
-if ($surveys) {
-    include(__DIR__ . '/templates/manage_survey_table.php');
-} else {
-    echo html_writer::tag('div', get_string('nosurveysfound', 'local_moodle_survey'), ['class' => 'alert alert-info']);
+display_page($surveys);
+
+/**
+ * Initializes the page context and resources.
+ */
+function initialize_page() {
+    global $PAGE;
+
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_url(new moodle_url('/local/moodle_survey/manage_survey.php'));
+    $PAGE->set_title(get_string('managesurvey', 'local_moodle_survey'));
+
+    $PAGE->requires->css(new moodle_url('/local/moodle_survey/css/styles.css'));
+    $PAGE->requires->js(new moodle_url('/local/moodle_survey/js/tabs.js'));
+    $PAGE->requires->js(new moodle_url('/local/moodle_survey/js/forms.js'));
 }
 
-echo $OUTPUT->footer();
+/**
+ * Retrieves the filter parameters from the request.
+ *
+ * @return array
+ */
+function get_filters() {
+    $search = optional_param('search', '', PARAM_RAW_TRIMMED);
+    $status = optional_param('status', '', PARAM_ALPHA);
+    $surveycategory = optional_param('category', '', PARAM_ALPHANUMEXT);
+    $createdon = optional_param('createdon', '', PARAM_RAW_TRIMMED);
+
+    return [
+        'search' => $search,
+        'status' => $status,
+        'surveycategory' => $surveycategory,
+        'createdon' => $createdon,
+    ];
+}
+
+/**
+ * Fetches surveys based on the provided filters.
+ *
+ * @param array $filters
+ * @return array
+ */
+function fetch_surveys($filters) {
+    $dbhelper = new \local_moodle_survey\model\survey();
+    return $dbhelper->get_surveys($filters);
+}
+
+/**
+ * Displays the survey management page.
+ *
+ * @param array $surveys
+ */
+function display_page($surveys) {
+    global $OUTPUT;
+
+    // Include the HTML for the survey management interface
+    include(__DIR__ . '/templates/manage_survey_header.php');
+
+    if ($surveys) {
+        include(__DIR__ . '/templates/manage_survey_table.php');
+    } else {
+        echo html_writer::tag('div', get_string('nosurveysfound', 'local_moodle_survey'), ['class' => 'alert alert-info']);
+    }
+
+    echo $OUTPUT->footer();
+}
