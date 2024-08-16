c07246e58f5583ccf6e42749e9f20b9287eb1d40
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-08-16 21:55:18 +0530

    feat(*): added the moodle pie chart

--- fill_survey/survey_analysis.php
+++ fill_survey/survey_analysis.php
@@ -3,8 +3,9 @@
 require_once('../../../config.php');
 require_login();
 
+use core\chart_pie;
+use core\chart_series;
 $context = context_system::instance();
-
 $id = required_param('id', PARAM_INT);
 $survey = $DB->get_record('cc_surveys', ['id' => $id], '*', MUST_EXIST);
 $PAGE->set_title(get_string('selanalysis', 'local_moodle_survey'));
@@ -12,7 +13,7 @@ $PAGE->requires->js(new moodle_url('/local/moodle_survey/js/forms.js'));
 echo $OUTPUT->header();
 echo render_survey_analysis_title();
 echo render_survey_instruction($survey);
-echo render_survey_insights();
+echo render_survey_insights($OUTPUT, $CFG);
 
 
 function render_survey_analysis_title() {
@@ -39,24 +40,38 @@ function render_survey_instruction($survey) {
 }
 
 
-function render_survey_insights() {
+function render_survey_insights($OUTPUT, $CFG) {
     $iconurl = new \moodle_url('/local/moodle_survey/pix/arrow-down.svg');
-    $html = html_writer::start_tag('div', array('class' => '', 'id'=>"accordion"));
+    $html = html_writer::start_tag('div', array('id'=>"accordion"));
         $html .= html_writer::start_tag('div', array('class' => 'accordion active'));
             $html .= html_writer::start_tag('div', array('class' => 'accordion-header accordion-header-section survey-about-container'));
                 $html .= html_writer::start_tag('img', array('src' => $iconurl, 'class' => 'accordion-icon'));
                 $html .= html_writer::tag('h3', 'Insights', array('class' => 'survey-about-heading mb-0 ml-2'));
             $html .= html_writer::end_tag('div');
-            $html .= html_writer::start_tag('div', array('class' => 'accordion-body pl-5'));
-                $html .= render_survey_analysis_chart();
+            $html .= html_writer::start_tag('div', array('class' => 'accordion-body'));
+                $html .= html_writer::start_tag('div', array('class' => 'survey-analysis-chart-body'));
+                    for ($i=0; $i < 3; $i++) { 
+                        $html .= render_survey_analysis_chart($OUTPUT, $CFG);
+                    }
+                $html .= html_writer::end_tag('div');
             $html .= html_writer::end_tag('div');
         $html .= html_writer::end_tag('div');
     $html .= html_writer::end_tag('div');
     return $html;
 }
 
-function render_survey_analysis_chart() {
-    return "here";
+function render_survey_analysis_chart($OUTPUT, $CFG) {
+    $html = html_writer::start_tag('div', array('class' => 'survey-analysis-chart'));
+        $CFG->chart_colorset = ['#F47A29', '#FFF0E6', '#FFF'];
+        $pieChart = new chart_pie();
+        $pieChartData = [rand(0,100), rand(0,100), rand(0,100)];
+        $series = new chart_series('Insights', $pieChartData);
+        $pieChart->add_series($series);
+        $pieChart->set_title('Survey Data');
+    $horizontalBarChartHtml = $OUTPUT->render_chart($pieChart, false);
+    $html .= $horizontalBarChartHtml;
+    $html .= html_writer::end_tag('div');
+    return $html;
 }
 
 echo $OUTPUT->footer();
